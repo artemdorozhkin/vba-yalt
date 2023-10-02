1a9f3688a3ece5627cc1cd9ab8d922aa1ff42385
Artem Dorozhkin <aa.dorozhkin@ya.ru>
2023-10-03 02:02:26 +0300

    remove module name from sumbols

--- src/language-features/SymbolManager.ts
+++ src/language-features/SymbolManager.ts
@@ -7,34 +7,32 @@ export class SymbolManager {
       if (token.isLib()) {
         this.getSymbolsFromTokens(token.modules, output);
       } else if (token.isModule() || token.isClass()) {
-        const moduleParent = this.buildSymbol(token);
-        output.push(moduleParent);
         this.getSymbolsFromTokens(token.types, output);
         this.getSymbolsFromTokens(token.enums, output);
         this.getSymbolsFromTokens(token.methods, output);
         this.getSymbolsFromTokens(token.properties, output);
         const variables = token.variables.map(this.buildSymbol);
-        moduleParent.children.push(...variables);
+        output.push(...variables);
       } else if (token.isType()) {
         const typeParent = this.buildSymbol(token);
         const fields = token.members.map(this.buildSymbol);
         typeParent.children.push(...fields);
-        output[0].children.push(typeParent);
+        output.push(typeParent);
       } else if (token.isEnum()) {
         const enumParent = this.buildSymbol(token);
         const members = token.members.map(this.buildSymbol);
         enumParent.children.push(...members);
-        output[0].children.push(enumParent);
+        output.push(enumParent);
       } else if (token.isMethod()) {
         const methodParent = this.buildSymbol(token);
         const variables = token.variables.map(this.buildSymbol);
         methodParent.children.push(...variables);
-        output[0].children.push(methodParent);
+        output.push(methodParent);
       } else if (token.isProperty()) {
         const propParent = this.buildSymbol(token);
         const variables = token.variables.map(this.buildSymbol);
         propParent.children.push(...variables);
-        output[0].children.push(propParent);
+        output.push(propParent);
       }
     });
   }
